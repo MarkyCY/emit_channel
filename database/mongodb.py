a3b223ef79a536cdf27aa744c94d80d21e0bbdc3
Marcos J. Cardenas P. <oranmarcos@gmail.com>
2024-06-19 16:54:29 -0400

    Add mongodb function

---
+++ database/mongodb.py
@@ -0,0 +1,7 @@
+import os
+import motor.motor_asyncio
+
+async def get_db():
+    mongo_uri = os.getenv('MONGO_URI')
+    client = motor.motor_asyncio.AsyncIOMotorClient(mongo_uri)
+    return client.emit_bot
