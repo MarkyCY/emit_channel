d812afe2be528fa9b42cc2fb37a00e0af3c1af0d
Marcos J. Cardenas P. <oranmarcos@gmail.com>
2024-06-19 16:52:40 -0400

    Add main

---
+++ main.py
@@ -0,0 +1,26 @@
+from pyrogram import Client
+from dotenv import load_dotenv
+
+load_dotenv()
+
+import asyncio
+import os
+
+# Create a new Pyrogram client
+api_id = os.getenv('API_ID')
+api_hash = os.getenv('API_HASH')
+bot_token = os.getenv('BOT_TOKEN')
+
+plugins = dict(root="plugins")
+app = Client('my_bot',api_id=api_id, api_hash=api_hash, bot_token=bot_token, plugins=plugins)
+
+#Definir la función main()
+async def main():
+    await app.start()
+    print('*Bot Online.')
+
+#Iniciar Proceso de la función main()
+print("Bot Starting")
+loop: asyncio.AbstractEventLoop = asyncio.get_event_loop_policy().get_event_loop()
+loop.create_task(main())
+loop.run_forever()
