188d3e16a2844f177932b6deaf3489a602e37853
Marcos J. Cardenas P. <oranmarcos@gmail.com>
2024-06-20 23:35:31 -0400

    Add remove msg command

--- plugins/commands/channels.py
+++ plugins/commands/channels.py
@@ -88,3 +88,21 @@ async def rm_channel_command(app: Client, message: Message):
     ])
 
     await app.send_message(message.chat.id, f"Deseas eliminar este canal a la lista de canales?", reply_markup=markup)
+
+
+@Client.on_message(filters.command('rm_msg'))
+async def rm_msg_command(app: Client, message: Message):
+
+    chat_id = message.chat.id
+
+    if message.reply_to_message is None:
+        await app.send_message(message.chat.id, "Por favor responde a un mensaje para eliminarlo")
+        return
+
+    markup = InlineKeyboardMarkup([
+        [
+            InlineKeyboardButton('Si', callback_data=f'rm_msg_{chat_id}_{message.reply_to_message.id}'),
+        ],
+    ])
+
+    await app.send_message(message.chat.id, f"Deseas eliminar este mensaje de todas partes?", reply_markup=markup)
