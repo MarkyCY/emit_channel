bb6b9c7074d569441b519bab4bf1082eb91c61dc
Marcos J. Cardenas P. <oranmarcos@gmail.com>
2024-06-28 18:51:24 -0400

    Modify to really show info

--- plugins/commands/show_info.py
+++ plugins/commands/show_info.py
@@ -1,24 +1,22 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
 
+
+
 @Client.on_message(filters.command('info'))
-async def info_command(app: Client, message: Message):
-    print(message)
-    markup = InlineKeyboardMarkup([
-        [
-            InlineKeyboardButton('Info', callback_data='info')
-        ],
-    ])
-    await app.send_message(message.chat.id, 'Info', reply_markup=markup)
-
-
-@Client.on_callback_query(filters.regex('info'))
-async def info_callback(app: Client, query: Message):
-    print(query)
-    await app.send_message(query.message.chat.id, query.from_user.id)
-
-
-@Client.on_message(filters.command('info_chat'))
 async def info_chat_command(app: Client, message: Message):
-    chat = await app.get_chat(message.chat.id)
-    print(chat)
+    text = f"""
+Información:
+
+Chat Title: <code>{message.chat.title}</code>
+Chat ID: <code>{message.chat.id}</code>
+"""
+    if message.reply_to_message:
+        text += f"""
+Reply Message ID: <code>{message.reply_to_message.id}</code>
+"""
+    #btns = InlineKeyboardMarkup([
+    #    [InlineKeyboardButton('Hacer chat Principal', callback_data=f'principal_{message.chat.id}')]
+    #])
+
+    await app.send_message(message.chat.id, text)#, reply_markup=InlineKeyboardMarkup(btns))
