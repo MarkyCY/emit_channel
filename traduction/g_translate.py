76c733f8a872d90d2876d4abce94b1bc660d9461
Marcos J. Cardenas P. <oranmarcos@gmail.com>
2024-08-08 11:56:48 -0400

    change function name

--- traduction/g_translate.py
+++ traduction/g_translate.py
@@ -1,7 +1,7 @@
 from deep_translator import GoogleTranslator
 import asyncio
 
-async def async_translate(description, source_language, target_language):
+async def google_translate(description, source_language, target_language):
     loop = asyncio.get_event_loop()
     # Ejecuta la función translate de manera asíncrona
     translated_description = await loop.run_in_executor(
